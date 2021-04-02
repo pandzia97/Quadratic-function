8e95a35cb2693938c25d0de46430881d0464c06f
pandzia97 <andzia.okarmus@gmial.com>
2021-04-02 21:58:35 +0200

    Edited gitignore and added a, b, c in Main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,16 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        final Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a: ");
+        double a = scanner.nextDouble();
+        System.out.println("Enter b: ");
+        double b = scanner.nextDouble();
+        System.out.println("Enter c: ");
+        double c = scanner.nextDouble();
     }
 }
