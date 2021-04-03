860155af5dabe614c469b12aabe24f1070c84a2d
pandzia97 <andzia.okarmus@gmial.com>
2021-04-03 14:46:14 +0200

    Added code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,5 +12,20 @@ public class Main {
         double b = scanner.nextDouble();
         System.out.println("Enter c: ");
         double c = scanner.nextDouble();
+
+        double delta = b * b - 4 * a * c;
+        double rootDelta = Math.sqrt(b);
+
+        if (delta < 0) {
+            System.out.println("No zeros");
+        } else if (delta == 0) {
+            double x0 = -b / 2 * a;
+            System.out.println(x0);
+        } else if (delta > 0) {
+            double x1 = -b - rootDelta / 2 * a;
+            double x2 = -b + rootDelta / 2 * a;
+            System.out.println(x1);
+            System.out.println(x2);
+        }
     }
 }
